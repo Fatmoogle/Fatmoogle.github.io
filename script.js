b26ba4aa0949f240ba55823fdd94ae27f4f5ee7f
Alex Varela <alexvar93@gmail.com>
2021-11-11 17:21:36 -0500

    fixed script file bug that happened after git pull

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     const modalBg = $(".modal-bg");
     const card = $(".card");
     const appBtn = document.querySelector("#app");
@@ -6,64 +7,6 @@ $(document).ready(function() {
     const oldImageUrl = "./Assets/Images/Profile_pic.jpg"
     let imageUrl = "";
     const contactPage = document.querySelector(".contact2");
-
-    var modalBg = document.querySelector(".modal-bg");
-    var modalClose = document.querySelector(".modal-close");
-    var modal1 = document.querySelector(".modal1");
-    var modal2 = document.querySelector(".modal2");
-    var modal3 = document.querySelector(".modal3");
-    var modal4 = document.querySelector(".modal4");
-    var modalHeader = document.querySelector("h2");
-    var modalParagraph = document.querySelector("p");
-    var appBtn = document.querySelector(".applicationBtn");
-    var gitBtn = document.querySelector(".githubBtn");
-
-
-    console.log(gitBtn);
-
-    /* Modal functions for each portfolio object */
-
-    modal1.addEventListener('click', function(){
-        console.log("modal 1");
-        modalBg.classList.add("bg-active");
-        modalHeader.textContent = "Coding Quiz!";
-        modalParagraph.textContent = "Fun and silly game created using JavaScript to test your knowledge on basic coding ideas. Answer correctly before time runs out!";
-        appBtn.setAttribute("onclick", "window.location.href='https://fatmoogle.github.io/Coding-Quiz/';");
-        gitBtn.setAttribute("onclick", "window.location.href='https://github.com/Fatmoogle/Coding-Quiz';");
-    })
-
-    modal2.addEventListener('click', function(){
-        console.log("modal 2");
-        modalBg.classList.add("bg-active");
-        modalHeader.textContent = "Dinner Dice";
-        modalParagraph.textContent = "A mobile friendly application created to generate random recipes for drinks and food using two API’s. Simply choose a filter for each and then generate your recipe!";
-        appBtn.setAttribute("onclick", "window.location.href='https://victoriagfield.github.io/Dinner_Dice/';");
-        gitBtn.setAttribute("onclick", "window.location.href='https://github.com/victoriagfield/Dinner_Dice';");
-    })
-
-    modal3.addEventListener('click', function(){
-        console.log("modal 3");
-        modalBg.classList.add("bg-active");
-        modalHeader.textContent = "Password Generator";
-        modalParagraph.textContent = "Application to generate random password between 8 and 128 characters in length.";
-        appBtn.setAttribute("onclick", "window.location.href='https://fatmoogle.github.io/Password-Generator/';");
-        gitBtn.setAttribute("onclick", "window.location.href='https://github.com/Fatmoogle/Password-Generator';");
-    })
-
-    modal4.addEventListener('click', function(){
-        console.log("modal 4");
-        modalBg.classList.add("bg-active");
-        modalHeader.textContent = "Weather Dashboard";
-        modalParagraph.textContent = "Weather application that allows user to search for the current and 5 day forecast of a city.";
-        appBtn.setAttribute("onclick", "window.location.href='https://fatmoogle.github.io/Weather-Dashboard/';");
-        gitBtn.setAttribute("onclick", "window.location.href='https://github.com/Fatmoogle/Weather-Dashboard';");
-    })
-
-    /*  Function to close modal window  */
-
-    modalClose.addEventListener('click', function(){
-        modalBg.classList.remove("bg-active");
-    })
 
     card.on("click", function () {
         const className = $(this).attr('class');
@@ -134,23 +77,19 @@ $(document).ready(function() {
     $(".modal-close").on("click", function() {
         modalBg.removeClass("bg-active");
     });
-
     /* Functions to change large picture background image in about me section */
-
     $("#profilePic2").hover(() => {
         imageUrl = "./Assets/Images/profil_pic2.jpg";
         $("#profilePic1").css('background-image', 'url(' + imageUrl + ')');
     }, () => {
         $("#profilePic1").css('background-image', 'url(' + oldImageUrl + ')');
     });
-
     $("#profilePic3").hover(() => {
         imageUrl = "./Assets/Images/profile_pic3.jpg";
         $("#profilePic1").css('background-image', 'url(' + imageUrl + ')');
     }, () => {
         $("#profilePic1").css('background-image', 'url(' + oldImageUrl + ')');
     });
-
     $("#profilePic4").hover(() => {
         imageUrl = "./Assets/Images/profile_pic4.jpg";
         $("#profilePic1").css('background-image', 'url(' + imageUrl + ')');
@@ -158,20 +97,15 @@ $(document).ready(function() {
         $("#profilePic1").css('background-image', 'url(' + oldImageUrl + ')');
     });
 
-
-
     /* Interesection Observer for the contact page */
-
     const options = {
         threshold: .3
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
-
             /*Conditional was needed here because the observer was firing upon page load when it should not be. Now it adds and removes classes ONLY 
             if the entry is intersecting. Entry contains alot of information, so console log to see all it includes.*/
-            
             if (entry.isIntersecting) {
                 entry.target.classList.add('in-viewport');
                 // console.log(entry.isIntersecting);
@@ -190,11 +124,8 @@ $(document).ready(function() {
             // alert("Hey");
         })
     }, options);
-
     observer.observe(contactPage);
 });
-
-
 // entries.forEach(entry => {
 //     if (entry.intersectionRatio > 0) {
 //       entry.target.classList.add('in-viewport');
